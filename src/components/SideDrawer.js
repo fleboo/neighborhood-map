ab9b35f1ce48a2724bacdb7dc38e89f08c917335
Felix Leboo <felixlemi@gmail.com>
2018-10-29 20:19:06 -0500

    Added SideDrawer component

---
+++ src/components/SideDrawer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import '../App.css';
+import VenueList from '../components/VenueList';
+
+const sideDrawer = (props) => {
+  let sideDrawerClass = props.sideDrawerVisible ? "ShowDrawer" : "HideDrawer";
+  return (
+    <div className={sideDrawerClass}>
+      <div className="CloseIconWrapper">
+        <div className="CloseIcon" aria-label="Close">
+          <span onClick={props.closeIconClick} type="button" aria-hidden="true">&times;</span>
+        </div> 
+      </div>
+      <div className="SearchInputWrapper">
+        <input 
+          type="text" 
+          placeholder="Filter Coffee Shops"
+          onChange={(event) => {props.changed(event.target.value)}} />
+      </div>
+      <VenueList venues={props.venues} listItemClick={props.listItemClick} />
+    </div>
+  );
+}
+
+export default sideDrawer;
