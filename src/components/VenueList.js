6f7ebba32b837ae889c2433206c63719283c7319
Felix Leboo <felixlemi@gmail.com>
2018-10-29 22:24:04 -0500

    Display message when no matching venues

--- src/components/VenueList.js
+++ src/components/VenueList.js
@@ -4,8 +4,10 @@ import '../App.css';
 import ListItem from './ListItem';
 
 const venueList = (props) => {
+  
   return (
     <ol tabIndex="0" className="VenueList">
+      {props.venues.length <= 0 && <p className="Matching">No matching coffee shops</p>}
       {props.venues && 
         props.venues.map( (venue, index) => ( 
           <ListItem key={index} venue={venue} clicked={() => { props.listItemClick(venue) }} /> 
